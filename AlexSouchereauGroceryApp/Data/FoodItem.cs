f19d5fa09940c5571fd6fa9a7fc8058ea8acaee5
aSouchereau <98470000+aSouchereau@users.noreply.github.com>
2022-03-28 23:53:38 -0400

    filled grocery store razor componend

--- AlexSouchereauGroceryApp/Data/FoodItem.cs
+++ AlexSouchereauGroceryApp/Data/FoodItem.cs
@@ -5,10 +5,9 @@ namespace AlexSouchereauGroceryApp.Data
     public class FoodItem
     {
         public int Quantity { get; set; }
-        public int FoodName { get; set; }
+        public string FoodName { get; set; }
 
         public string FoodGroup { get; set; }
-        public int Calories { get; set; }
         public decimal Price { get; set; }
 
     }
