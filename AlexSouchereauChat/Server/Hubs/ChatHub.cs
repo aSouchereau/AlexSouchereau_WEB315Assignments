b41dd4362c724daeac38fcb319f1b1e9411d0483
aSouchereau <98470000+aSouchereau@users.noreply.github.com>
2022-04-17 21:35:29 -0400

    added type indicator

--- AlexSouchereauChat/Server/Hubs/ChatHub.cs
+++ AlexSouchereauChat/Server/Hubs/ChatHub.cs
@@ -5,9 +5,18 @@ namespace BlazorWebAssemblySignalRApp.Server.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string name, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", name, message);
+        }
+
+        public async Task SendTypeIndicator(string name)
+        {
+            await Clients.All.SendAsync("ReceiveTypeIndicator", name);
+        }
+        public async Task SendRemoveTypeIndicator(string name)
+        {
+            await Clients.All.SendAsync("ReceiveRemoveTypeIndicator", name);
         }
     }
 }
